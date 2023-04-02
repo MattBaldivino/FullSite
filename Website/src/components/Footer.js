d67cc888f3e83b0025b21cd0a8ee3282a3033135
MattBaldivino <mattbaldivino@gmail.com>
2023-04-02 11:28:45 -0700

    daily commit

--- Website/src/components/Footer.js
+++ Website/src/components/Footer.js
@@ -11,7 +11,7 @@ import axios from 'axios';
 		const [recipient_email, setEmail] = useState("");
 		const [subject, setSubject] = useState("");
 		const [message, setMessages] = useState("");
-		const url = "/send_email";
+		const url = "https://mattb-site.herokuapp.com/send_email";
 
 		function sendMail(){
 			if(recipient_email && subject && message){
