1ca9aa82d29275dde9ee75ec8258c825c03de08c
MattBaldivino <mattbaldivino@gmail.com>
2023-04-02 00:50:13 -0700

    nightly commit

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 require("dotenv").config();
@@ -6,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 
+app.use(express.static(path.resolve(_dirname, "./Website/build")));
 app.use(express.json({limit: "25mb"}));
 app.use(express.urlencoded({limit: "25mb"}));
 app.use((req, res, next)=>{
